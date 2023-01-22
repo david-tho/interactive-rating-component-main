f049f8638b2fa011ec134d2e88e070e82bbe3eec
rayzii-gh <117836000+rayzii-gh@users.noreply.github.com>
2023-01-22 14:58:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const rateState = document.querySelector(".rating-state");
+const thanksState = document.querySelector(".thanks-state");
+const submitBtn = document.querySelector(".submit-btn");
+const rateBtn = document.querySelectorAll(".rate-btn");
+const rateNumber = document.querySelector(".rate-number");
+
+rateNumber.innerHTML = 0;
+
+const handleClick = function (e) {
+  rateBtn.forEach((val) => {
+    if (e.target.innerHTML === val.innerHTML) {
+      val.classList.toggle("active");
+    }
+  });
+  rateNumber.innerHTML = e.target.innerHTML;
+};
+
+const OpenState = function () {
+  rateState.classList.add("hidden");
+  thanksState.classList.remove("hidden");
+};
+
+rateBtn.forEach((val) => val.addEventListener("click", handleClick));
+submitBtn.addEventListener("click", OpenState);
